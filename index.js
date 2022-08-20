a504d4ecae177d72577fa1cab6005d4ddf98c851
adamichelle <adamichelllle@gmail.com>
2022-08-20 04:09:02 -0400

    Drilling Further into The Response

--- index.js
+++ index.js
@@ -6,6 +6,49 @@ const notionClient = new Client({
     auth: process.env.NOTION_AUTH_TOKEN
 });
 
+const _extractContactDetails= async (filteredContactsArray) => {
+    let details;
+    if (filteredContactsArray) {
+        const neededPageAndPropertiesIds= filteredContactsArray.map((contactObject) => ({
+            pageId: contactObject.id,
+            phoneNumberPropertyId: contactObject.properties['Phone Number'].id,
+            namePropertyId: contactObject.properties['Name'].id,
+            reachOutByPropertyId: contactObject.properties['Reach out by'].id
+        }))
+
+        const promises = neededPageAndPropertiesIds.map(async (pageAndPropertyId) => {
+            const { pageId, phoneNumberPropertyId, namePropertyId, reachOutByPropertyId } = pageAndPropertyId;
+
+            const phoneNumberProperty = await notionClient.pages.properties.retrieve({
+                page_id: pageId,
+                property_id: phoneNumberPropertyId
+            });
+
+            const reachOutByProperty = await notionClient.pages.properties.retrieve({
+                page_id: pageId,
+                property_id: reachOutByPropertyId
+            });
+
+            const nameProperty = await notionClient.pages.properties.retrieve({
+                page_id: pageId,
+                property_id: namePropertyId
+            });
+
+            return {
+                phoneNumber: phoneNumberProperty.phone_number,
+                reachOutBy: reachOutByProperty.select.name,
+                name: nameProperty.results[0]?.title.text.content
+            };
+        })
+
+        details = await Promise.all(promises);
+    } else {
+        throw new Error('You did not provide any list to extract data from.');
+    }
+
+    return details;
+};
+
 (async () => {
     const response = await notionClient.databases.query({
         database_id: process.env.NOTION_DATABASE_ID,
@@ -17,5 +60,6 @@ const notionClient = new Client({
         },
     });
     
-    console.log(response);
+    const details = await _extractContactDetails(response.results);
+    console.log(details)
 })()
