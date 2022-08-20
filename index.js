3d04cc7c2f26076513a071e3de639734214ef740
adamichelle <adamichelllle@gmail.com>
2022-08-20 04:03:30 -0400

    Getting data from our Contacts database

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
-const dotenv = require("dotenv")
-const { Client } = require("@notionhq/client")
+const dotenv = require("dotenv");
+const { Client } = require("@notionhq/client");
   
 dotenv.config()
 const notionClient = new Client({
     auth: process.env.NOTION_AUTH_TOKEN
-})
+});
+
+(async () => {
+    const response = await notionClient.databases.query({
+        database_id: process.env.NOTION_DATABASE_ID
+    });
+    
+    console.log(response);
+})()
